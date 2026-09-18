fb7606c4cfaff7cc100b455bc83947b1bad6bce3
zerokit-769 <haidliralimutan@gmail.com>
2026-09-19 03:47:28 +0700

    Update bypass.py

--- bypass.py
+++ bypass.py
@@ -3,7 +3,7 @@ from playwright.async_api import async_playwright
 
 # === KONFIGURASI PROXY (UBAH DI SINI) ===
 # Ganti dengan alamat proxy kamu, format: http://host:port
-PROXY_SERVER = "http://206.135.56.50:8080"
+PROXY_SERVER = "https://103.145.176.162:8080"
 # =======================================
 
 TARGET_URL = "https://drama.center"
